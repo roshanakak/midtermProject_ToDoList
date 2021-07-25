7488ed4423bc327817ffaa6522d8acdfae478399
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-25 17:49:45 +0000

    add user dropdown menu

---
+++ public/scripts/user-avatar.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+  $('.avatar').on("click", function () {
+
+    const menu = $('#user-dropdown-content');
+    menu.toggle(200)
+
+    // menu.css({"background-color" : "blue"})
+
+
+  })
+
+
+})
