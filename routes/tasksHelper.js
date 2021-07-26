fffa0459361a39599835b362317305635e91b1f7
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-07-26 19:24:10 -0400

    after uninstallin imdb

--- routes/tasksHelper.js
+++ routes/tasksHelper.js
@@ -3,29 +3,41 @@ module.exports = (taskName) => {
   let state = 'Not sure';
   
   // search in the books
-  let books = require('google-books-search');
-  let options = {
-    field: 'title',
-    type: 'books',
-    order: 'relevance',
-  };
-  books
-    .search(taskName, options, function(error, results) {
-      if (!error) {
-        if (results.length === 0) {
-          state = 'Not Matched';
-        } else {
-          results.forEach(element => {
-            if (element.title.includes(taskName)) {
-              state = 'Matched';
-              return;
-            }
-          });
-        }
-      } else {
-        state = 'Error';
-      }
-      console.log(state);
-    });
+  // let books = require('google-books-search');
+  // let options = {
+  //   field: 'title',
+  //   type: 'books',
+  //   order: 'relevance',
+  // };
+  // books
+  //   .search(taskName, options, function(error, results) {
+  //     if (!error) {
+  //       if (results.length === 0) {
+  //         state = 'Not Matched';
+  //       } else {
+  //         results.forEach(element => {
+  //           if (element.title.includes(taskName)) {
+  //             state = 'Matched';
+  //             return;
+  //           }
+  //         });
+  //       }
+  //     } else {
+  //       state = 'Error';
+  //     }
+  //     console.log(state);
+  //   });
+
+
+  // search in movies
+  let imdb = require('imdb');
+  imdb('tt3659388', function(err, data) {
+    if (err) {
+      console.log(err.stack);
+    }
+    if (data) {
+      console.log(data);
+    }
+  });
 
 };
