baeea8969cf9cbbcac36396f0a99db9a82ef4664
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-27 00:18:17 +0000

    add edit modal box

--- public/scripts/category-dropdown.js
+++ public/scripts/category-dropdown.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
 
-  $('.fas.fa-sort-down').on("click", function () {
+  $('.fas.fa-caret-down').on("click", function () {
 
     const menu = $('.category-contents');
     menu.toggle(200)
 
-    $(".fas.fa-sort-down").css({"visibility" : "hidden", "order" : "2"})
+    $(".fas.fa-caret-down").css({"visibility" : "hidden", "order" : "2"})
     $(".fas.fa-caret-up").css({"visibility" : "visible", "order" : "1"})
   })
 
@@ -15,8 +15,7 @@ $(document).ready(function () {
     menu.toggle(200)
 
     $(".fas.fa-caret-up").css({"visibility" : "hidden", "order" : "2"})
-    $(".fas.fa-sort-down").css({"visibility" : "visible", "order" : "1"})
-
+    $(".fas.fa-caret-down").css({"visibility" : "visible", "order" : "1"})
 
   })
 
