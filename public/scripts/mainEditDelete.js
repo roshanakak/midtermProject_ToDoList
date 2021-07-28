a31f18a9eb8ad5a635222fb6663001ff21e3818c
Yi Tung Leung <tungtungleung233@hotmail.com>
2021-07-28 19:30:17 +0000

    add modal backdrop, change background image, center main page

--- public/scripts/mainEditDelete.js
+++ public/scripts/mainEditDelete.js
@@ -4,28 +4,52 @@ $(document).ready(function() {
   let deleteElements = $(".fa-trash-alt");
 
   const editTask = function() {
+    $(".edit-modal-root").css({
+      "background-color" : "rgba(0,0,0,0.2)",
+      "position" : "absolute",
+      "top" : "0",
+      "left" : "0",
+      "right" : "0",
+      "bottom" : "0",
+      "z-index" : "1"
+    })
     $("#edit-modal").css({"visibility" : "visible"})
+
   };
 
   $("#edit-modal-close").on("click", function () {
-    $("#edit-modal").css({"visibility" : "hidden"})
+    $(".edit-modal-root").css({"z-index" : "-1"})
 
     $("#caret-up-edit-task").css({"visibility" : "hidden"})
     $("#caret-down-edit-task").css({"visibility" : "hidden"})
   })
 
 
+
   const deleteTask = function() {
+    $(".delete-modal-root").css({
+      "background-color" : "rgba(0,0,0,0.2)",
+      "position" : "absolute",
+      "top" : "0",
+      "left" : "0",
+      "right" : "0",
+      "bottom" : "0",
+      "z-index" : "1"
+    })
     $("#delete-modal").css({"visibility" : "visible"})
+
   };
 
   $("#delete-modal-close").on("click", function () {
-    $("#delete-modal").css({"visibility" : "hidden"})
+    $(".delete-modal-root").css({"z-index" : "-1"})
+    // $("#delete-modal").css({"visibility" : "hidden"})
 
   })
 
   $("#cancel-deletion-button").on("click", function () {
-    $("#delete-modal").css({"visibility" : "hidden"})
+    $(".delete-modal-root").css({"z-index" : "-1"})
+
+    // $("#delete-modal").css({"visibility" : "hidden"})
 
   })
 
